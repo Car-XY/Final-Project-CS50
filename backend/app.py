53d91f53c8c0a7e73b44d646fb4c2d98ff0ca2fc
Car-XY <caryanan.li@gmail.com>
2026-08-17 14:18:56 -0400

    SQlite3 setup
    Setup sqlite3 to handle database queries and also got /login running. Need to fix apology.html and layout.html tho

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import os
+import sqlite3
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -18,6 +19,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Configure my database
+conn = sqlite3.connect("goal_tracker.db")
+conn.row_factory = sqlite3.Row  # lets you access columns by name, like CS50's db did
 
 @app.after_request
 def after_request(response):
@@ -53,26 +57,26 @@ def login():
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # # Query database for username
-        # rows = db.execute(
-        #     "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        # )
+        # Query database for username
+        rows = conn.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
-        # # Ensure username exists and password is correct
-        # if len(rows) != 1 or not check_password_hash(
-        #     rows[0]["hash"], request.form.get("password")
-        # ):
-        #     return apology("invalid username and/or password", 403)
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        #session["user_id"] = rows[0]["id"]
+        session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("index[temp].html")
+        return render_template("login.html")
 
 
 # remove this when shipping
